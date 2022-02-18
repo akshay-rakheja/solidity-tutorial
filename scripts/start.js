d79ede120f517f536318c45ab9b408c82a5ba5ef
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-02-18 11:56:05 -0500

    adding deploy script

--- scripts/start.js
+++ scripts/start.js
@@ -1,12 +1,31 @@
 async function main() {
+  const [owner, somebodyElse] = await hre.ethers.getSigners();
   const keyboardsContractFactory = await hre.ethers.getContractFactory("Keyboards");
   const keyboardsContract = await keyboardsContractFactory.deploy();
   await keyboardsContract.deployed();
 
   console.log("Contract deployed to:", keyboardsContract.address);
-
-  const keyboards = await keyboardsContract.getKeyboards();
+  
+  //getkeyboard txn without creating one.
+  let keyboards = await keyboardsContract.getKeyboards();
   console.log("We got the keyboards!", keyboards);
+
+  //keyboard txn
+  const keyboardTxn1 = await keyboardsContract.create("A really great keyboard!");
+  await keyboardTxn1.wait();
+
+  //txn 2
+  const keyboardTxn2 = await keyboardsContract.connect(somebodyElse).create("A really shitty keyboard!");
+  await keyboardTxn2.wait();
+
+
+  //getkeyboard txn after creating one
+  keyboards = await keyboardsContract.getKeyboards();
+  console.log("We got the keyboards!", keyboards);
+
+  //from the other user
+  keyboards = await keyboardsContract.connect(somebodyElse).getKeyboards();
+  console.log("And as somebody else!", keyboards);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
